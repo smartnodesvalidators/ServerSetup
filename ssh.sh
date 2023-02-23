adc7b0a24252ca69b2f0d16ae2524f2d460c5302
smartnodesvalidators <84850351+smartnodesvalidators@users.noreply.github.com>
2023-02-23 13:24:09 +0000

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,3 +1,4 @@
 sudo sed -i "s/#Port 22/Port 1609/" /etc/ssh/sshd_config
 sudo sed -i "s/Port 22/Port 1609/" /etc/ssh/sshd_config
+sudo sed -i "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config
 sudo service ssh restart
