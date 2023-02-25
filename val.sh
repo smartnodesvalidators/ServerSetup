34cd4e6dfcf3f4830021f2cb20a19f0233a7e0e4
smartnodesvalidators <84850351+smartnodesvalidators@users.noreply.github.com>
2023-02-25 20:59:08 +0000

    Update val.sh

--- val.sh
+++ val.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y
 sudo apt-get install git make wget curl unzip jq lz4 -y
 sudo apt-get install fail2ban -y
 sudo apt-get install mlocate -y
-sudo apt install net-tools open-vm-tools -y
+# sudo apt install net-tools open-vm-tools -y
 sudo updatedb
 
 sudo su -c "echo 'fs.file-max = 65536' >> /etc/sysctl.conf"
