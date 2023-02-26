4a646eaa41feff8e1d45c9b23d69626c9fe3e690
smartnodesvalidators <84850351+smartnodesvalidators@users.noreply.github.com>
2023-02-26 17:48:04 +0000

    Update fw.sh

--- fw.sh
+++ fw.sh
@@ -1,9 +1,1 @@
-sudo ufw allow 1609/tcp
-sudo ufw allow 1317/tcp
-sudo ufw allow 80/tcp
-sudo ufw allow 443/tcp
-sudo ufw allow 26657/tcp
-sudo ufw allow 9100/tcp
-sudo ufw allow 26660/tcp
-sudo systemctl enable ufw
-sudo systemctl start ufw
+
