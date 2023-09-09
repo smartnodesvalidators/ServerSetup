5aff0bc98d77b8d65d4a6cf40bd4ff04f47d4df1
smartnodesvalidators <84850351+smartnodesvalidators@users.noreply.github.com>
2023-09-09 19:54:51 +0100

    Create netdata.sh

---
+++ netdata.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --nightly-channel --claim-token EZXdG5qW9Dr1FGOlkm1xZrIAzFsR_43ZfAv39v2gfSNFbb0pzYJIsnqEXELU8ZdTRcTIshUm55pjhKvaqKck16YpJ7Icr7s-e-DymNJS8T56j34JP-RQ7rz9xGBqG6nBfk7glGg --claim-rooms d6b21782-02c0-4f0b-b27f-87f6e948f000 --claim-url https://app.netdata.cloud
