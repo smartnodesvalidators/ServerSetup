4043d29693fc565045ddc250a60cfbcf229bfc89
smartnodesvalidators <84850351+smartnodesvalidators@users.noreply.github.com>
2023-09-20 18:55:18 +0100

    Create netdatainstall.sh

---
+++ netdatainstall.sh
@@ -0,0 +1,1 @@
+wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --nightly-channel --claim-token XNrn1yRXZUyVOQE9S8aGJ8Ac2tajBV3xvZE3no-oZLXGkRc3gBjKmw_HyP2cLdFaYVZtyhsFwb5diXMUnliPBLOYa7y09wA18YyZKBWawRC1k3376zq_rhCXXRcsnXC9ycVjUJI --claim-rooms d6b21782-02c0-4f0b-b27f-87f6e948f000 --claim-url https://app.netdata.cloud
