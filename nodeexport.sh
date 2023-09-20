ae38bcd4d92ce7f56f8d3e9d7ddc7c8760800245
smartnodesvalidators <84850351+smartnodesvalidators@users.noreply.github.com>
2023-09-20 19:03:55 +0100

    Update nodeexport.sh

--- nodeexport.sh
+++ nodeexport.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
 tar xvf node_exporter-1.3.1.linux-amd64.tar.gz
 cd node_exporter-1.3.1.linux-amd64
