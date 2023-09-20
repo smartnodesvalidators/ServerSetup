d23f5c4be513c7d1d519609e02cde01decc50a09
smartnodesvalidators <84850351+smartnodesvalidators@users.noreply.github.com>
2023-09-20 19:02:33 +0100

    Create nodeexport.sh

---
+++ nodeexport.sh
@@ -0,0 +1,8 @@
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+tar xvf node_exporter-1.3.1.linux-amd64.tar.gz
+cd node_exporter-1.3.1.linux-amd64
+sudo cp node_exporter /usr/local/bin
+cd ..
+rm -rf ./node_exporter-1.3.1.linux-amd64
+sudo useradd --no-create-home --shell /bin/false node_exporter
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
